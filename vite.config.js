44496fb8aab0fd7332a7322f29ccf111e8a40ab4
Tatiana Naumova <110552243+Tatiana190389@users.noreply.github.com>
2024-08-09 13:53:24 +0300

    fixed vite.config

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    base: '/News_Feed',
+    plugins: [react()],
+});
